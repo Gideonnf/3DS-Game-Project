29ad240203f45dc877eb9ac108aa7c341822d123
Gideon Francis <gideonnfrancis999@gmail.com>
2026-06-21 21:15:39 +0800

    removed ignored files

--- GameProject/build/StateManager.d
+++
@@ -1,6 +0,0 @@
-StateManager.o: \
- C:/Users/gideo/Documents/PersonalProjects/3DSGame/GameProject/source/Core/StateManager.cpp \
- C:/Users/gideo/Documents/PersonalProjects/3DSGame/GameProject/source/Core/StateManager.hpp \
- C:/Users/gideo/Documents/PersonalProjects/3DSGame/GameProject/source/Singleton.hpp
-C:/Users/gideo/Documents/PersonalProjects/3DSGame/GameProject/source/Core/StateManager.hpp:
-C:/Users/gideo/Documents/PersonalProjects/3DSGame/GameProject/source/Singleton.hpp:
